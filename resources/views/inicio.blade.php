ae944b70a9c5bc14ba81a7de65815fd9949b3456
SardinasJL <sardinasjoseluis1@gmail.com>
2024-11-11 01:52:41 -0400

    Update inicio.blade.php
    Cambios en el formato de la carátula

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -6,8 +6,8 @@
         <img src="{{url("imagenes/uesa.png")}}" class="img-fluid">
         <h2>"DESARROLLO DE UNA BASE DE DATOS Y GENERACIÓN DE CÓDIGOS QR PARA LA GESTIÓN DEL REGISTRO DE NIÑOS Y DATOS
             DE PADRES O TUTORES EN EL KINDER DE LA UNIDAD EDUCATIVA SANTA ANA"</h2>
-        <h3>AUTOR(A): EST. HERRERA SALAS NICOL ROCIO</h3>
-        <h3>TUTOR: LIC. JOSUÉ JUEL ÁNGELO SIVILA</h3>
+        <!---<h3>AUTOR(A): EST. HERRERA SALAS NICOL ROCIO</h3>
+        <h3>TUTOR: LIC. JOSUÉ JUEL ÁNGELO SIVILA</h3>--->
     </div>
 
 @endsection
